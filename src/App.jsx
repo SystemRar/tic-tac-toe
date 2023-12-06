2ac3b5c00d26f2eefe999e5ea770d8cce0af1bc6
Ian Covaliuc <ian.covaliuc@gmail.com>
2023-12-06 20:59:52 +0200

    Creating a board

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
 
-function App() {
+export default function App() {
   return (
-    <></>
+      <>
+        <div className="board-row">
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+        </div>
+        <div className="board-row">
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+        </div>
+        <div className="board-row">
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+          <button className={'square'}>X</button>
+        </div>
+      </>
   )
 }
-
-export default App
