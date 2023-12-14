ec996b4fb739b660ac1c61314a14ad492d3ce01c
Ian Covaliuc <ian.covaliuc@gmail.com>
2023-12-14 21:23:13 +0200

    Take out the Board component separately

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,7 @@
-import './App.css'
+import './App.css';
 
-import {useMemo, useState} from "react";
-import PropTypes from "prop-types";
+import {useState} from "react";
+import Board from "../Board/Board.jsx";
 import Moves from "../Moves/Moves.jsx";
 
 export default function App() {
@@ -18,78 +18,8 @@ export default function App() {
 
     return (
         <div className={'game'}>
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-            </div>
+            <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             <Moves setCurrentMove={setCurrentMove} history={history}/>
         </div>
     )
-}
-
-function Board({ xIsNext, squares, onPlay }) {
-    const winner = useMemo(() => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }, [squares]);
-
-    function handleClick(i) {
-        if (squares[i] || winner) {
-            return;
-        }
-
-        const nextSquares = squares.slice();
-        xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O';
-
-        onPlay(nextSquares);
-    }
-
-    let status;
-
-    if (winner) {
-        status = `Winner: ${winner}`;
-    } else {
-        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-        <>
-            <div className="status"><p>{status}</p></div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
-        </>
-    )
-}
-
-Board.propTypes = {
-    xIsNext: PropTypes.bool,
-    squares: PropTypes.arrayOf(PropTypes.string),
-    onPlay: PropTypes.func
 }
